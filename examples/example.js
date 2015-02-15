a66e559661e7c71ea22c759c5be7e55f41bc096d
Mike Chelen <michael.chelen@gmail.com>
2015-02-15 16:16:43 -0500

    cleanup

--- examples/example.js
+++ examples/example.js
@@ -1,13 +1,8 @@
 var npc = require('../lib/nativeplantcenter');
-
-
 var foo = new npc();
 
 foo.keyword("bittersweet",function(result){
-
-  //console.log("blah");
-//  console.log("foo "+JSON.stringify(result,null,"  "));
-
+  console.log("foo "+JSON.stringify(result,null,"  "));
 })
 
 
