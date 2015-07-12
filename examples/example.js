a7780cadbe39bcbe748ea36ddc6dd5666361bf41
Mike Chelen <michael.chelen@gmail.com>
2015-07-12 17:26:38 -0400

    deprecate jsonpath

--- examples/example.js
+++ examples/example.js
@@ -1,8 +1,9 @@
 var npc = require('../lib/nativeplantcenter');
 var foo = new npc();
 
-foo.keyword("bittersweet",function(result){
-  console.log("search result: "+JSON.stringify(result,null,"  "));
+foo.keyword("divaricatus",function(result){
+//  console.log("search result: "+JSON.stringify(result,null,"  "));
+  console.log("search result: "+result);
 })
 
 
