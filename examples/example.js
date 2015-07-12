f3bd64ce6b4fe1f0ae2eca181225afaa3e1dbc6f
Mike Chelen <michael.chelen@gmail.com>
2015-07-12 18:38:26 -0400

    sorta working

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,9 @@ var npc = require('../lib/nativeplantcenter');
 var foo = new npc();
 
 foo.keyword("bittersweet",function(result){
-  console.log("search result: "+JSON.stringify(result,null,"  "));
+//  console.log("search result: "+JSON.stringify(result,null,"  "));
+  
+  console.log(result);
 })
 
 
